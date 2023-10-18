93f600e92b1c2baa033cd1c8bcf97980879bbbd8
Stavrevski54 <148367686+Stavrevski54@users.noreply.github.com>
2023-10-18 18:53:00 +0200

    Update myClass.java

--- Lecture3/myClass.java
+++ Lecture3/myClass.java
@@ -1,24 +1,47 @@
 public class myClass {
-	static class myNumber{
-		private int num;
-		public int num2;
-		public myNumber(){
-			num=0;
-			num2=100;
-		}
-		int getNum(){
-			return num;
-		}
-		void setNum(int i){
-			num=i; 
-			num2=10*i;
-		}
-	}
-	public static void main(String[]args) {
-			myNumber A= new myNumber();
-			System.out.println(A.getNum());
-			A.setNum(10);
-			System.out.println(A.getNum());
-			System.out.println(A.num2); 
-	}
+    static class myNumber {
+        private int num;
+        public int num2;
+        
+        public myNumber() {
+            num = 0;
+            num2 = 100;
+        }
+        
+        public myNumber(int num, int num2) {
+            this.num = num;
+            this.num2 = num2;
+        }
+
+        int getNum() {
+            return num;
+        }
+
+        void setNum(int i) {
+            num = i;
+            num2 = 10 * i;
+        }
+
+        void description() {
+            System.out.println("num: " + num);
+            System.out.println("num2: " + num2);
+        }
+
+        float getAverage() {
+            return (num + num2) / 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        myNumber A = new myNumber();
+        System.out.println(A.getNum());
+        A.setNum(10);
+        System.out.println(A.getNum());
+        System.out.println(A.num2);
+
+        myNumber mb = new myNumber(13, 10);
+        System.out.println("Using constructor with parameters:");
+        mb.description();
+        System.out.println("Average: " + mb.getAverage());
+    }
 }
