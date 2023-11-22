b7e5dae4a6e72e1d4a381fe521e193bf6b2a6c5d
Stavrevski54 <148367686+Stavrevski54@users.noreply.github.com>
2023-11-22 18:59:05 +0100

    Create Test.java

---
+++ Lecture 9/Assignment 1/Test.java
@@ -0,0 +1,19 @@
+public class Test { 
+	String s;
+	
+	public Test() { 
+		this.s = "Programming languages"; 
+	} 
+	
+	public void method1() { 
+		System.out.println("Value of s: " + s); 
+	} 
+	
+	public void method2(int n) { 
+		System.out.println("Value of n: " + n); 
+	} 
+	
+	private void method3() { 
+		System.out.println("Private method is invoked"); 
+	} 
+} 
