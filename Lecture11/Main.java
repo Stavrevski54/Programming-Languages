b28be964fc45aa623bbb133c969fafbd835d362f
Stavrevski54 <148367686+Stavrevski54@users.noreply.github.com>
2023-12-06 18:57:16 +0100

    Create Main.java

---
+++ Lecture11/Main.java
@@ -0,0 +1,14 @@
+package assignment;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(2, 3, 4, 5, 2);
+        System.out.println("Collect method returns a set");
+        Set<Integer> squareSet = numbers.stream().map(x -> x * x).collect(Collectors.toSet());
+        System.out.println(squareSet);
+    }
+}
